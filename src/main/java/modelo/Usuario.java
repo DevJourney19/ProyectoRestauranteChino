2ebcc62f7a4880df0d9373f2bb51b29b84de9118
DevJourney19 <F4r3ver@gmail.com>
2024-08-23 02:26:31 -0500

    Agregué una pequeña estructura para el patrón MVC

---
+++ src/main/java/modelo/Usuario.java
@@ -0,0 +1,63 @@
+package modelo;
+
+public class Usuario {
+	private int id;
+	private String nombre;
+	private String apellido;
+	private String rol;
+	private String nombreUsuario;
+	private String contrasenia;
+	
+	public Usuario() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getRol() {
+		return rol;
+	}
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+}
---
+++ src/main/webapp/vista/mozo.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Mozo</title>
+
+<link href="css/general_css.css" rel="stylesheet">
+<link href="css/mozo_css.css" rel="stylesheet">
+</head>
+<body>
+
+	<div class="restaurante_nombre">Noche en Pekín 北京之夜</div>
+	<div class="pedidos_div">PEDIDOS</div>
+
+	<button class="seleccionar_mesa">Seleccionar Mesa</button>
+	
+	
+
+
+</body>
+</html>
