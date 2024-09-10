c9a36faa291ad6121fa932d42b7e566a7f2ded61
josh-ara <stdnt.full@gmail.com>
2024-09-09 20:29:29 -0500

    se agrego el modal para mensajes de los administradores

--- src/main/webapp/vista/administrador/fragmentos/sidebar.js
+++
@@ -1,7 +0,0 @@
-const toggler = document.querySelector(".toggler-btn");
-toggler.addEventListener("click", function () {
-  document.querySelector("#sidebar").classList.toggle("collapsed");
-  document.querySelector("#logo").classList.toggle("collapsed");
-});
-
-// Obtener path y colocar una clase active al side que corresponda
--- src/main/webapp/vista/trabajadores/fragmentos/sidebar.js
+++
@@ -1,4 +0,0 @@
-const buttonToggle = document.querySelector("#toggle");
-buttonToggle.addEventListener("click", function() {
-	document.querySelector("#sidebar").classList.toggle("collapsed");
-});
