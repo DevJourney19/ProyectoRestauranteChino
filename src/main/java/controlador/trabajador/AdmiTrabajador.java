fdd53a7b85497181c10aed0b66bf9a14653d3f67
Henry Sixto Chavez <80175027+henrysixtoch@users.noreply.github.com>
2024-11-10 20:21:36 -0500

    Update AdmiTrabajador.java

--- src/main/java/controlador/trabajador/AdmiTrabajador.java
+++ src/main/java/controlador/trabajador/AdmiTrabajador.java
@@ -1,6 +1,8 @@
 package controlador.trabajador;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import datos.DaoRol;
 import datos.DaoTrabajador;
@@ -15,21 +17,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import modelo.Rol;
 import modelo.Trabajador;
 
-import java.io.IOException;
-
 @WebServlet(name = "AdmiTrabajador", urlPatterns = { "/AdmiTrabajador" })
 public class AdmiTrabajador extends HttpServlet {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		processRequest(request, response);
 	}
 
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		processRequest(request, response);
@@ -41,6 +43,15 @@ public class AdmiTrabajador extends HttpServlet {
 		DaoRol rolDao = new DaoRolImpl();
 		List<Trabajador> lista = trabajadorDao.consultar();
 		List<Rol> listaRol = rolDao.consultar();
+		
+		// Verificar si hay un título de búsqueda
+		if (request.getParameter("tituloSearch") != null) {
+				String titulo = request.getParameter("tituloSearch");
+					// Filtrar el menú basado en el título
+				lista = lista.stream().filter(m -> m.getNombre().toLowerCase().contains(titulo.toLowerCase()))
+						.collect(Collectors.toList()); // Guardar el resultado del filtrado
+		}				
+				
 		request.setAttribute("trabajadores", lista);
 		request.setAttribute("rol", listaRol);
 		String archivo = "vista/administrador/usuarios/usuarios.jsp";
