56751ce45e86b66b13302300bc5596af4eda4d94
Henry Sixto Chavez <80175027+henrysixtoch@users.noreply.github.com>
2024-11-10 20:17:46 -0500

    Update DaoTrabajador.java

--- src/main/java/datos/DaoTrabajador.java
+++ src/main/java/datos/DaoTrabajador.java
@@ -5,6 +5,7 @@ import modelo.Trabajador;
 //Vas a llamar a la interface Dao con el tipo Usuario
 public interface DaoTrabajador extends Dao<Trabajador>{
 	Trabajador validarUsuario(String usuario, String password);
+	List<Trabajador> filtrar(String titulo);
 }
 
 
